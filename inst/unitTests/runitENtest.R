1c4ebd82176d370e1b2ed83dcb0d1857e9e2c205
phgrosjean <phgrosjean@sciviews.org>
2015-12-06 17:06:11 +0100

    Introducing a failing test for verification

--- inst/unitTests/runitENtest.R
+++ inst/unitTests/runitENtest.R
@@ -58,6 +58,6 @@ test_checkId <- function() {
     msg = "cycleNbr() extracts cycle number")
 
   typ <- c("s", "s", "a", "a", "s", "s", "a", "s")
-  checkIdentical(typ, typeId(newIds),
+  checkIdentical(cyc, typeId(newIds),
     msg = "typeId() extracts type Id")
 }
